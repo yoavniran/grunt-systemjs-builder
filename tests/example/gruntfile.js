4103c293df0dbbd5b4eac4172cf614d413f106f7
yoav niran <yoavniran@gmail.com>
2015-11-01 10:07:58 +0200

    added example project to test the task

---
+++ tests/example/gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+
+    grunt.loadTasks("../../tasks");
+
+    grunt.initConfig({
+
+        systemjs: {
+            demo: {
+                options: {
+                    configFile: "src/main/javascript/config.js",
+                    minify: false,
+                    sfx: false
+                },
+                files: [{
+                    src: "app/demo/app.js",
+                    dest: "src/main/javascript/min/demo/demo.min.js"
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["systemjs"]);
+};
+
+
